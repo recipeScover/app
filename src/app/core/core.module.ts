68e67c3b36628bb44920131c2ca26b1b8885ac5b
Luka Milosevic <luka.milosevic@aesys.tech>
2021-02-01 12:46:54 +0100

    Application layout scaffold

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,27 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainComponent } from './layouts/main/main.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [MainComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  exports: [MainComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() private readonly coreModule: CoreModule) {
