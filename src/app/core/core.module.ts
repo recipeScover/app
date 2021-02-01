ce7ed7f33fdd819be2206e6a0d9de80cdfe200ea
Luka Milosevic <luka.milosevic@aesys.tech>
2021-02-01 12:35:55 +0100

    Import check for core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() private readonly coreModule: CoreModule) {
+    if (this.coreModule) {
+      throw new Error('The core module can be only used inside the app module');
+    }
+  }
+}
