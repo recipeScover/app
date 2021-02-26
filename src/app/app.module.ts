43b0af8992e0a0c48abd8e8be7847edf624721cf
angelodif <angelo.diflorio@aesys.tech>
2021-02-26 12:00:25 +0100

    Add login basic, without style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,29 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
+
+
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+
+import { AuthService } from "./shared/services/auth-service.service";
+import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+
+    VerifyEmailComponent,
 
   ],
   imports: [
@@ -26,8 +46,11 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     HttpClientModule,
     LayoutModule,
     MatProgressSpinnerModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,13 @@ export const environment = {
   production: true,
   apiUrl: 'https://www.themealdb.com/api/json/v1/1'
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyDcWzBiMlwKl1GwMFUDvHvzZqlMvNXDl_0",
+  authDomain: "angular-portfolio-521bb.firebaseapp.com",
+  databaseURL: "https://angular-portfolio-521bb-default-rtdb.firebaseio.com",
+  projectId: "angular-portfolio-521bb",
+  storageBucket: "angular-portfolio-521bb.appspot.com",
+  messagingSenderId: "972889278105",
+  appId: "1:972889278105:web:7de43f10f02d5fba9c9a08",
+  measurementId: "G-R2CM65ETDX"
+};
